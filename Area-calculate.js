5b63938525c90df12de4ce609cbc6530dbe7870f
Utso-Sarkar <devutso428@gmail.com>
2023-08-25 20:40:43 +0600

    added done up

--- Area-calculate.js
+++ Area-calculate.js
@@ -3,4 +3,18 @@ function triangleAreaCalcute(){
     const baseFieldText =baseField.value;
     const base = parseFloat(baseFieldText);
     console.log(base);
+
+    // get triengle height
+    const heightField =document.getElementById('traingle-height');
+    const heightFieldText =heightField.value;
+    const height =parseFloat(heightFieldText);
+    console.log(height);
+
+    const area = 0.5 * base * height;
+    console.log(area);
+
+    // area span calculation
+    const areaSpan=document.getElementById("area-span");
+    
+
 }
