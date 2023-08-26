2d7a438dc8f1f615e193b87c9c62c740670bc7c6
Utso-Sarkar <devutso428@gmail.com>
2023-08-26 22:55:54 +0600

    added the git part

--- Area-calculate.js
+++ Area-calculate.js
@@ -15,6 +15,49 @@ function triangleAreaCalcute(){
 
     // area span calculation
     const areaSpan=document.getElementById("area-span");
+    areaSpan.innerText=area;
     
 
 }
+
+// rectangle-calculation 
+function rectangleArea(){
+    const rectWidth=document.getElementById('rectangle-width');
+    const reactValue =rectWidth.value;
+    const width=parseFloat( reactValue);
+    console.log(width);
+
+
+    // react length
+    const rectLength=document.getElementById('rectangle-length');
+    const reactValueLength =rectLength.value;
+    const length=parseFloat( reactValueLength);
+    console.log(length);
+
+    // area calculation 
+const rectArea =width * length;
+console.log(rectArea);
+
+// show area 
+const areaSpanRect =document.getElementById('area-RectSpan');
+areaSpanRect.innerText=rectArea;
+}
+
+// para field calculation
+function paraCalculationArea (){
+    const base =getInputValue('para-width');
+    console.log(base);
+    const height =getInputValue('para-length')
+    const area =base* height;
+    const areaPara =document.getElementById('area-peraSpan');
+    areaPara.innerText=area;
+}
+
+// reuseable section for all 
+function getInputValue(fieldId){
+    const inputField =document.getElementById(fieldId);
+    const inputText =inputField.value;
+    const value=parseFloat(inputText);
+    return value;
+}
+
